b4aa71677589af9cd7cee6521a4cf9d3c72e1dda
Bing Xie <bxie@locomote.com>
2017-02-14 15:27:48 +1100

    Test log format and namespace

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,3 +3,13 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 
 resque_config = YAML.load_file(rails_root + '/config/resque.yml')
 Resque.redis = resque_config[rails_env]
+
+
+# Resque.logger.formatter = Resque::QuietFormatter.new
+Resque.logger.formatter = Resque::VerboseFormatter.new
+# Resque.logger.formatter = Resque::VeryVerboseFormatter.new
+
+
+Resque.redis.namespace = "learn-resque"
+
+
